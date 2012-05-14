3cc60fb574309cfd3fd15c2c7f1b9f2cae841bb0
Juha T Hakkanen <jhakkane@wop-1.cs.helsinki.fi>
2012-05-14 17:58:27 +0300

    abc

--- Harjoitustyo/src/Harjoitustyo/Harjoitustyo.java
+++ Harjoitustyo/src/Harjoitustyo/Harjoitustyo.java
@@ -15,5 +15,11 @@ public class Harjoitustyo {
      */
     public static void main(String[] args) {
         // TODO code application logic here
+        
+        System.out.println("ABC");
+        
+        
+        
+        
     }
 }
